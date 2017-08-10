8d0b65b8d06ecc454f1d5b1b2774be21386b20de
Zhenghao Jia <cherie-xia@hotmail.com>
2017-08-10 21:02:42 +0800

    upload
    version 2017/8/10

--- portfolio.html/portfolio.js
+++ portfolio.html/portfolio.js
@@ -1,6 +1,11 @@
  $(document).ready(function(){
 	$('body').scrollspy({target: '.navbar-fixed-top'});
 	$('#portfolio img').magnify();
+	$("#send").click(function(e) {
+		e.preventDefault();
+		window.location.reload();//刷新当前页面.
+	});
+
 });
 
 
